6182dee8525fc3e5205f7ddca13faeeb8b6bf79e
Gabriel <gabrielrozendodacosta@gmail.com>
2025-03-11 20:42:49 -0300

    feat: Coloca o Lombok na classe para abstração

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/entity/MissoesEntity.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/entity/MissoesEntity.java
@@ -2,11 +2,17 @@ package dev.java10x.CadastroDeNinjas.Missoes.entity;
 
 import dev.java10x.CadastroDeNinjas.Ninjas.entity.NinjaEntity;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class MissoesEntity {
 
     @Id
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/entity/NinjaEntity.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/entity/NinjaEntity.java
@@ -2,12 +2,18 @@ package dev.java10x.CadastroDeNinjas.Ninjas.entity;
 
 import dev.java10x.CadastroDeNinjas.Missoes.entity.MissoesEntity;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 // Entity transforma uma classe em uma entidade no banco de dados
 @Entity
 @Table(name = "tb_cadastro")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class NinjaEntity {
 
     @Id
@@ -25,36 +31,5 @@ public class NinjaEntity {
     @JoinColumn(name = "missoes_id") // Foreing key ou chave estrangeira
     private MissoesEntity missoes;
 
-    public NinjaEntity(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
 
-    public NinjaEntity() {
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
